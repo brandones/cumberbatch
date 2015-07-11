aa3e276276b03ad52e36a88d53a2e9a67a75edf6
Brandon Istenes <brandonesbox@gmail.com>
2015-07-11 11:40:56 -0700

    Add pypi files

---
+++ setup.py
@@ -0,0 +1,18 @@
+""" setup.py
+
+    Copyright 2015 Socos LLC
+"""
+
+from distutils.core import setup
+setup(
+  name = 'cumberbatch',
+  packages = ['cumberbatch'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Generate names similar to Benedict Cumberbatch.',
+  author = 'Brandon Istenes',
+  author_email = 'brandonesbox@gmail.com',
+  url = 'https://github.com/brandones/cumberbatch',
+  download_url = 'https://github.com/brandones/cumberbatch/tarball/0.1',
+  keywords = ['names', 'testing', 'fixtures', 'generation', 'generator', 'benedict', 'cumberbatch'],
+  classifiers = [],
+)
